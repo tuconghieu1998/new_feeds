f4c10b7578ac87cdb25b2526fdc81baea847023b
Hieu <tuconghieu1998@gmail.com>
2023-05-28 10:35:00 +0700

    show data comment

--- lib/modules/home/widgets/comment_item.dart
+++ lib/modules/home/widgets/comment_item.dart
@@ -4,6 +4,7 @@ import 'package:news_feed/common/widgets/stateless/divide.dart';
 import 'package:news_feed/modules/home/models/comment.dart';
 import 'package:news_feed/themes/app_colors.dart';
 import 'package:news_feed/themes/styles_text.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class CommentItem extends StatelessWidget {
   const CommentItem({super.key, this.data});
@@ -17,15 +18,16 @@ class CommentItem extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-            child: Avatar(size: 50,),
+            child: Avatar(size: 50, url: data?.user?.avatar?.url,),
           ),
           Container(width: MediaQuery.of(context).size.width - 95,
             child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: 5,),
-            Text("Eugene Rodriquez", style: AppStylesText.body17.copyWith(fontSize: 22),),
-            Text("2 hours ago", style: AppStylesText.caption13.copyWith(color: AppColor.unselectItems),),
+            Text((data?.user?.firstName != null ? "${data!.user!.firstName} " : "") + (data?.user?.lastName ?? ""), style: AppStylesText.body17.copyWith(fontSize: 22),),
+            Text(timeago.format(data?.createdAt != null ? DateTime.parse(data?.createdAt ?? "") : DateTime.now()), style: AppStylesText.caption13.copyWith(color: AppColor.unselectItems
+            ),),
             Text(
               data?.content ?? "",
               style: AppStylesText.body17.copyWith(fontSize: 17, fontWeight: FontWeight.w400),
